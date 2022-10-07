3fb45cf7570be178ea76f15fa7c0f39fc2e3e925
Rohit verma <28vrohit@gmail.com>
2022-10-07 17:42:01 +0530

    Update App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import axois from 'axios';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+    this.base64String = ''
+  }
+
+  componentWillMount() {
+    axois.get('http://localhost:8080')
+      .then((res) => {
+        console.log(res.data)
+        this.setState({ data: res.data })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Image uploading react</h1>
+        {
+          this.state.data.map((res) => (
+            this.base64String = btoa(String.fromCharCode(...new Uint8Array(res.img.data))),
+            console.log('rv', this.base64String[0]),
+            <div>{res.name}</div>
+          ))
+        }
+      </div>
+    );
+  }
+};
 
 export default App;
+
